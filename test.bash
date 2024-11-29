64ab22b9ad7be1925fd40c74d8950f8c6c132936
23C1130 <s23c1130sm@s.chibakoudai.jp>
2024-11-29 15:44:18 +0900

    [Fix] test.bashの修正

--- test.bash
+++ test.bash
@@ -17,27 +17,29 @@ echo -e "-i\n/tmp/test_data.txt\n-k\n:\n-e" | ./counter > /tmp/test_output.txt
 grep b:2 /tmp/test_output.txt
 out=$?
 
-[ "$out" == 0 ] || ng "$LINENO"
+[ "$out" = 0 ] || ng "$LINENO"
 
 ### ヘルプ表示テスト ###
 echo -e "-h" | ./counter > /tmp/test_help.txt
 grep 文字をカウントして出力します /tmp/test_help.txt
 out=$?
 
-[ "$out" == 0 ] || ng "$LINENO"
+[ "$out" = 0 ] || ng "$LINENO"
 
 ### 入力エラーテスト（存在しない適当なファイルを入力する) ###
 
 echo -e "-i\n/tmp/nweokgjeiogneiopfj.txt\n-k\n:\n-e" | ./counter > /tmp/test_output.txt
 out=$?
 
-[ "$out" == 0 ] && ng "$LINENO"
+[ "$out" = 0 ] && ng "$LINENO"
 
 ### 区切り変更テスト（区切り文字を変更してみる)
 echo -e "-i\n/tmp/test_data.txt\n-k\n,\n-e" | ./counter > /tmp/test_output.txt
 grep b,2 /tmp/test_output.txt
 out=$?
 
-[ "$out" == 0 ] || ng "$LINENO"
+[ "$out" = 0 ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo TEST_OK
 
 exit $res
